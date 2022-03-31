5425f6d1db304f405afb1f9e95ae8ea24486e7f1
dbar2002 <duncanj.barnes@calbaptist.edu>
2022-03-31 01:43:59 -0700

    final commit

--- tst/AssassinManagerTest.java
+++ tst/AssassinManagerTest.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Put your comments here
+ * Duncan 3/31/22
  */
 
 //Test constructor
@@ -42,4 +42,149 @@ public class AssassinManagerTest {
         }catch (IllegalArgumentException e) {
         }
     }
+
+    @Test
+    public void graveyardContainsTest(){
+        List<String> list1= new ArrayList<String>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+
+        //test for front of ring
+        manager.printKillRing();
+        manager.kill("grayson");
+        manager.printGraveyard();
+        Assert.assertTrue(manager.graveyardContains("grayson"));
+
+        //test for middle of ring
+        manager.printKillRing();
+        manager.kill("Chris");
+        manager.printGraveyard();
+        Assert.assertTrue(manager.graveyardContains("Chris"));
+
+        //test for end of ring
+        manager.printKillRing();
+        manager.kill("dr.han");
+        manager.printGraveyard();
+        Assert.assertTrue(manager.graveyardContains("dr.han"));
+
+    }
+
+    @Test
+    public void constructorTest(){
+        List<String> list1= new ArrayList<String>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+
+        manager.printKillRing();
+    }
+
+    @Test
+    public void isGameOverTest(){
+        List<String> list1= new ArrayList<String>();
+        list1.add("Grayson");
+
+        AssassinManager manager = new AssassinManager(list1);
+
+        Assert.assertTrue(manager.isGameOver());
+    }
+
+    @Test
+    public void winnerTest(){
+        List<String> list1= new ArrayList<String>();
+        list1.add("Grayson");
+
+        AssassinManager manager = new AssassinManager(list1);
+
+        Assert.assertTrue(manager.winner().equals("Grayson"));
+    }
+
+    @Test
+    public void killRingContainsTest(){
+        List<String> list1= new ArrayList<String>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+
+        Assert.assertTrue(manager.killRingContains("chriS"));
+        Assert.assertTrue(manager.killRingContains("OCEAN"));
+        Assert.assertTrue(manager.killRingContains("grayson"));
+        Assert.assertTrue(manager.killRingContains("dr.hAn"));
+
+    }
+
+    @Test
+    public void killRingContainsNegtiveTest(){
+        List<String> list1= new ArrayList<String>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+
+
+        Assert.assertFalse(manager.killRingContains("bob"));
+        Assert.assertFalse(manager.killRingContains("sally"));
+        Assert.assertFalse(manager.killRingContains("grayso"));
+        Assert.assertFalse(manager.killRingContains("drhAn"));
+    }
+
+    @Test
+    public void killNegativeTest(){
+        try{
+            List<String> list1= new ArrayList<String>();
+            list1.add("Grayson");
+            AssassinManager manager = new AssassinManager(list1);
+            manager.kill("Grayso");
+            Assert.fail();
+        }catch (IllegalStateException e){
+        }
+    }
+
+    @Test
+    public void killNegativeTest2(){
+        try{
+            List<String> list1= new ArrayList<String>();
+            list1.add("Grayson");
+            list1.add("George");
+            AssassinManager manager = new AssassinManager(list1);
+            manager.kill("Grayso");
+            Assert.fail();
+        }catch (IllegalArgumentException e){
+        }
+    }
+
+    @Test
+    public void killTest(){
+        List<String> list1 = new ArrayList<String>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+        list1.add("Greg");
+
+        AssassinManager manager = new AssassinManager(list1);
+
+        manager.kill("Grayson");
+        Assert.assertFalse(manager.killRingContains("Grayson"));
+
+        manager.kill("Dr.han");
+        Assert.assertFalse(manager.killRingContains("dr.han"));
+
+        manager.kill("Greg");
+        Assert.assertFalse(manager.killRingContains("greg"));
+    }
+
+
 }
